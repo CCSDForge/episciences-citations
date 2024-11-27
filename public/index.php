395ff0b448ea14524771c61696898740af5c3cfc
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-11-27 20:44:01 +0100

    hide doctrine deprecation for now

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Kernel;
+use Doctrine\Deprecations\Deprecation;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+Deprecation::ignoreDeprecations();
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
