5d39680cd39b1e3b0d03142d4a8c3541dff66af5
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-12-07 18:41:17 +0100

    removed dead code
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- src/Repository/PaperReferencesRepository.php
+++ src/Repository/PaperReferencesRepository.php
@@ -38,29 +38,4 @@ class PaperReferencesRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return PaperReferences[] Returns an array of PaperReferences objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?PaperReferences
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
