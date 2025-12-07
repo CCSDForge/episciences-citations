978c63fb96fc273d3a02b98af78481acc4d3604f
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-12-07 18:40:33 +0100

    removed dead code
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- src/Repository/DocumentRepository.php
+++ src/Repository/DocumentRepository.php
@@ -38,29 +38,4 @@ class DocumentRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return Document[] Returns an array of Document objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('d.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Document
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
