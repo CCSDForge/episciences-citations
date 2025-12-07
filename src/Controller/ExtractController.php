c1bfece4954623a2d2f8a6457179b2c9d48f0d9f
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-12-07 18:21:09 +0100

    - fixed deprecations
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- src/Controller/ExtractController.php
+++ src/Controller/ExtractController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
